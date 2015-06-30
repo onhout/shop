e58732fff4f25b5d24467328da5d44f6acb05a9b
Peter Liu <onhout@gmail.com>
2015-06-29 23:33:40 -0700

    a bit of stuff

--- application/views/newproduct.php
+++ application/views/newproduct.php
@@ -19,48 +19,50 @@ include('adminnav.php');
         })
     })
 </script>
-<div class="row">
-    <div class="col-md-8 col-md-offset-2">
-        <form method="post" action="<?=base_url()?>products/add_new_product" enctype="multipart/form-data">
-            <div class="form-group">
-                <label for="name">Name:</label>
-                <input type="text" class="form-control" id="name" name="name">
-            </div>
-            <div class="form-group">
-                <label for="description">Description:</label>
-                <textarea rows="5" class="form-control" id="description" name="description"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="selectcategory">Categories</label>
-                <select id="selectcategory" class="form-control" name="category">
-                    <?php
-                    echo '<option selected></option>';
-                    foreach($categories as $category){
-                        echo '<option value="'.$category['name'].'">'.$category['name'].'</option>';
-                    }
-                    ?>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="addcategory">Or add new category:</label>
-                <input type="text" id="addcategory" class="form-control" name="category">
-            </div>
-            <div class="col-xs-6 form-group">
-                <label for="quantity">Quantity:</label>
-                <input type="number" id="quantity" name="quantity" class="form-control">
-            </div>
-            <div class="col-xs-6 form-group">
-                <label for="price">Price:</label>
-                <input type="text" id="price" name="price" class="form-control">
-            </div>
-            <div class="form-group">
-                <label for="uploadimage">Upload Images</label>
-                <input type="file" id="uploadimage" name="userfile[]" multiple="multiple">
-            </div>
-            <button type="submit" class="btn btn-primary">New Product</button>
-            <button type="reset" class="btn btn-default pull-right">Cancel</button>
-        </form>
-        <?=$this->session->flashdata('errors');?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <form method="post" action="<?=base_url()?>products/add_new_product" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" id="name" name="name">
+                </div>
+                <div class="form-group">
+                    <label for="description">Description:</label>
+                    <textarea rows="5" class="form-control" id="description" name="description"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="selectcategory">Categories</label>
+                    <select id="selectcategory" class="form-control" name="category">
+                        <?php
+                        echo '<option selected></option>';
+                        foreach($categories as $category){
+                            echo '<option value="'.$category['name'].'">'.$category['name'].'</option>';
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="addcategory">Or add new category:</label>
+                    <input type="text" id="addcategory" class="form-control" name="category">
+                </div>
+                <div class="col-xs-6 form-group">
+                    <label for="quantity">Quantity:</label>
+                    <input type="number" id="quantity" name="quantity" class="form-control">
+                </div>
+                <div class="col-xs-6 form-group">
+                    <label for="price">Price:</label>
+                    <input type="text" id="price" name="price" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="uploadimage">Upload Images</label>
+                    <input type="file" id="uploadimage" name="userfile[]" multiple="multiple">
+                </div>
+                <button type="submit" class="btn btn-primary">New Product</button>
+                <button type="reset" class="btn btn-default pull-right">Cancel</button>
+            </form>
+            <?=$this->session->flashdata('errors');?>
+        </div>
     </div>
 </div>
 
